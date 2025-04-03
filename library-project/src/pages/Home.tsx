c36eaa6e1d24525e6a6e903e1faa5d6318fa2920
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-03 19:44:48 -0300

    chore: initial request test

--- library-project/src/pages/Home.tsx
+++ library-project/src/pages/Home.tsx
@@ -1,6 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
-export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+interface Book {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors?: string[];
+    imageLinks?: {
+      thumbnail: string;
+    };
+  };
 }
+export const Home = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const fetched = useRef(false);
+  useEffect(() => {
+    if (fetched.current) return;
+    fetched.current = true;
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=${import.meta.env.VITE_GOOGLE_BOOKS_API}`
+        );
+        setBooks(response.data.items || []);
+        console.log(response.data)
+      } catch (error) {
+        console.error("Erro ao buscar livros:", error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Livros sobre Harry Potter</h1>
+        <ul>
+          {books.map((book) => (
+            <li key={book.id}>
+              <h3>{book.volumeInfo.title}</h3>
+              <p>{book.volumeInfo.authors?.join(", ")}</p>
+              {book.volumeInfo.imageLinks?.thumbnail && (
+                <img
+                  src={book.volumeInfo.imageLinks.thumbnail}
+                  alt={book.volumeInfo.title}
+                  width="100"
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+    </div>
+  );
+};
+
