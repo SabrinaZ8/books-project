4985ac8b15a87fccb34cfadc57842b5b93e18cbb
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-06 15:00:13 -0300

    feat: add new route listbooks

--- library-project/src/routes/index.tsx
+++ library-project/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Favorites } from '../pages/Favorites/Favorites';
 import { Home } from '../pages/Home/Home';
 import { ProductSingle } from '../pages/ProductSingle/ProductSingle';
+import { ListBooks } from '../pages/ListBooks/ListBooks';
 
 export const AppRoutes = () => {
     return (
@@ -10,6 +11,7 @@ export const AppRoutes = () => {
             <Route path="/" element={<Home />} />
             <Route path="/favorites" element={<Favorites />} />
             <Route path="/singleProduct/:id" element={<ProductSingle />} />
+            <Route path="/listbooks" element={<ListBooks />} />
           </Routes>
       </BrowserRouter>
     );
