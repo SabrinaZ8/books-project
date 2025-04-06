b45e209f24a08f94d92ade32d4b8352384d2ce16
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-06 20:13:45 -0300

    feat: listbooks page styling and scope

--- library-project/src/pages/ListBooks/ListBooks.tsx
+++ library-project/src/pages/ListBooks/ListBooks.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { BookSearch } from "../../components/BookSearch";
+import { NavBar } from "../Home/NavBar";
 
 interface Book {
   id: string;
@@ -37,22 +39,35 @@ export const ListBooks = () => {
   }, [query]);
 
   return (
-    <div className="flex mt-14 w-1/2 justify-center flex-col items-center">
-      <ul>
+    <div className="flex flex-col justify-center items-center bg-gray-100">
+      <div className="bg-[url(../../../src/assets/background/bg-image.webp)] w-full bg-cover bg-center">
+        <div className="bg-black/5 backdrop-blur-xs w-full flex flex-col justify-center items-center p-4">
+          <NavBar />
+          <BookSearch />
+        </div>
+      </div>
+      <i className="font-semibold text-sm text-dimgray mt-4 w-full px-4">
+        Resultados para: {query}
+      </i>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10 mt-5 px-4">
         {books.map((book) => (
-          <li key={book.id}>
-            <h3>{book.volumeInfo.title}</h3>
-            <p>{book.volumeInfo.authors?.join(", ")}</p>
+          <div
+            key={book.id}
+            className="shadow-xl p-2 cursor-pointer bg-white hover:bg-gainsboro transition-bg duration-300"
+          >
             {book.volumeInfo.imageLinks?.thumbnail && (
               <img
                 src={book.volumeInfo.imageLinks.thumbnail}
                 alt={book.volumeInfo.title}
                 width="100"
+                className="shadow-md/70"
               />
             )}
-          </li>
+            <h3 className="font-semibold">{book.volumeInfo.title}</h3>
+            <p className="text-sm">{book.volumeInfo.authors?.join(", ")}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
