bef4b8cb14bd9f1cc69b669bc253d01cbb3cb450
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-15 14:45:56 -0300

    feat: add styles e adjusts in request

--- library-project/src/pages/Home/components/TopAuthor.tsx
+++ library-project/src/pages/Home/components/TopAuthor.tsx
@@ -12,37 +12,39 @@ export const TopAuthor = () => {
   const [authors, setAuthors] = useState<AuthorInfo[]>([]);
 
   useEffect(() => {
-    const fetchAuthors = async (key: string) => {
+    const fetchAuthors = async () => {
       try {
-        const response = await axios.get(
-          `https://openlibrary.org/authors/${key}.json`
+        const responses = await Promise.all(
+          keyAuthors.map(async (author) => {
+            const res = await axios.get(`https://openlibrary.org/authors/${author}.json`);
+            return res.data;
+          })
         );
-        setAuthors((prev) => [...prev, response.data]);
+        setAuthors(responses);
       } catch (error) {
-        console.error("Erro ao buscar livros:", error);
+        console.error("Erro ao buscar autores:", error);
       }
     };
-
-    keyAuthors.map((author) => {
-         fetchAuthors(author);
-  })
+  
+    fetchAuthors();
   }, []);
+
 console.log(authors)
   return (
-    <div className="flex flex-col justify-center items-center bg-wheat">
+    <div className="h-1/2 bg-wheat p-10 rounded-lg">
       <div>
         <h2 className="title-h2">Top Autores</h2>
       </div>
-      <div className="grid grid-rows-3 grid-cols-2 gap-4 mb-10 mt-5 px-1 sm:x-4 w-3/4">
+      <div className="grid grid-rows-3 grid-cols-1 gap-4 my-10 items-center">
         {authors
           .map((author) => (
             <div
               className={`flex justify-start items-center p-2 cursor-pointer  bg-white hover:bg-gainsboro transition-bg duration-300`}
             >
-              <div className="flex items-center w-[170px] sm:w-[200px] h-[270px] sm:h-[300px] mr-4">
+              <div className="flex items-center fle mr-4">
                 <img
                   src={`https://covers.openlibrary.org/a/olid/${author.key.replace('/authors/', '')}-M.jpg`}
-                  className={`object-cover min-w-[170px] w-[170px] sm:w-[200px] h-[300px]  `}
+                  className={`w-[200px] h-[200px] min-w-[200px] object-cover`}
                 />
               </div>
               <div>
@@ -51,7 +53,6 @@ console.log(authors)
             </div>
           ))}
       </div>
-      <div></div>
     </div>
   );
 };
