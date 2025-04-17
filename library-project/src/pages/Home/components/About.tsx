3b11830da3034e9fbdbe5cafa6b837edd88177c0
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-17 15:44:07 -0300

    style: add more style in About component

--- library-project/src/pages/Home/components/About.tsx
+++ library-project/src/pages/Home/components/About.tsx
@@ -2,10 +2,10 @@ import woman from "../../../assets/svgs/woman-reading-animate.svg";
 
 export const About = () => {
   return (
-    <div className="flex justify-center items-center bg-wheat p-10">
-      <div className="w-1/2 ">
+    <div className="flex justify-center items-center bg-wheat p-10 flex-col lg:flex-row">
+      <div className={`lg:w-1/2`}>
         <h2 className="title-h2 my-10"> O que é a Open Library?</h2>
-        <p className="italic ">
+        <p className="italic">
           A Open Library é um projeto incrível criado pela organização sem fins
           lucrativos Internet Archive. A missão deles é bem simples: criar uma
           página na web para cada livro já publicado. Com milhões de títulos
@@ -20,7 +20,7 @@ export const About = () => {
           Acesse diretamente em:{" "}<a className="font-bold underline" href="https://openlibrary.org" target="_blank" rel="noopener noreferrer">openlibrary.org</a>
         </p>
       </div>
-      <div className="w-1/2 flex justify-end">
+      <div className="w-full md:w-1/2 flex justify-center lg:justify-end">
         <img src={woman} alt="" className="w-[80%]" />
       </div>
     </div>
