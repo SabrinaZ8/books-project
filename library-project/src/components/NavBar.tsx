85861404dc26162149a655d9373f426db5e74818
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-18 10:56:24 -0300

    feat: add NavLink in the foother

--- library-project/src/components/NavBar.tsx
+++ library-project/src/components/NavBar.tsx
@@ -1,20 +1,40 @@
 import { CiMenuFries } from "react-icons/ci";
 import { BiSolidBookHeart } from "react-icons/bi";
+import logo from "../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav className="flex w-full justify-between p-10 h-[105px] bg-darkslategray text-white">
-      <div>
-        <i>Logo</i>
-      </div>
-      <div className="flex items-center">
-        <div className="flex px-10">
-          <BiSolidBookHeart className="h-5 w-5 mr-1"/>
-          <p className="leading-none">wishlist</p>
-        </div>
+    <nav className="flex w-full justify-between items-center p-10 h-[105px] bg-darkslategray text-white">
+     
+      <NavLink to="/">
         <div>
-          <CiMenuFries />
+          <img
+            src={logo}
+            alt="Logo"
+            className="w-[70px] rounded-r-full rounded-l-full"
+          />
         </div>
+      </NavLink>
+
+      <div className="flex items-center">
+        <NavLink to="/">
+          <div className="flex px-10 items-center cursor-pointer">
+            <p className="leading-none">Início</p>
+          </div>
+        </NavLink>
+
+        <NavLink to="/favorites">
+        <div className="flex px-10 items-center">
+          <BiSolidBookHeart className="w-7 h-7 mr-1" />
+          <p className="leading-none">Favoritos</p>
+        </div>
+        </NavLink>
+
+        <div>
+          <CiMenuFries className="w-7 h-7" />
+        </div>
+
       </div>
     </nav>
   );
