5ff37b96d55af42b7094078ada869338f2e1b5aa
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-25 20:42:07 -0300

    feat: add button for remove favorite

--- library-project/src/pages/Favorites/Favorites.tsx
+++ library-project/src/pages/Favorites/Favorites.tsx
@@ -1,29 +1,54 @@
-import { NavBar } from "../../components/NavBar"
-import { useFavoritesContext } from "../../hooks/useFavoriteContext"
+import { NavBar } from "../../components/NavBar";
+import { useFavoritesContext } from "../../hooks/useFavoriteContext";
+import { AiFillDelete } from "react-icons/ai";
+import { FaRegEye } from "react-icons/fa";
+
 
 export const Favorites = () => {
-  const { favorites } = useFavoritesContext()
-  console.log(favorites)
+  const { favorites, removeFavorite } = useFavoritesContext();
+  console.log(favorites);
   return (
     <div>
       <NavBar />
       <div className="my-20 ml-[60px]">
-        <h1 className="text-6xl font-semibold font-playfair tracking-widest">Seus Favoritos</h1>
+        <h1 className="text-6xl font-semibold font-playfair tracking-widest">
+          Seus Favoritos
+        </h1>
       </div>
       <div className="p-10 flex flex-wrap">
         {favorites.map((favorite) => (
-          <div className="mx-5">
-            <div className="flex flex-col items-center justify-center">              
-              <img 
-                src={`https://covers.openlibrary.org/b/id/${favorite.cover_i ? favorite.cover_i : favorite.covers?.[0]}-L.jpg`} 
-                alt="" 
-                className="shadow-md/70" />
+          <div className="mx-5 p-2">
+            <div className="flex flex-col items-start justify-start">
+              <img
+                src={`https://covers.openlibrary.org/b/id/${
+                  favorite.cover_i ? favorite.cover_i : favorite.covers?.[0]
+                }-L.jpg`}
+                alt=""
+                className="shadow-md/70"
+              />
               <h2 className="text-xl font-semibold my-5">{favorite.title}</h2>
-              <h3>{favorite.author_name ? favorite.author_name : favorite.authors?.map((author) => author.name).join(", ")}</h3>
+              <h3>
+                {favorite.author_name
+                  ? favorite.author_name
+                  : favorite.authors?.map((author) => author.name).join(", ")}
+              </h3>
+            </div>
+            <div className="flex justify-start">
+              <button
+                type="button"
+                onClick={() => removeFavorite(favorite.key)}
+                className="flex items-center justify-between mr-10 font-semibold"
+              >
+                <AiFillDelete className="w-8 h-8" />
+                Excluir
+              </button>
+              <button className="">
+                <FaRegEye className="w-8 h-8"/>
+              </button>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
