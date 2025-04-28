e6e12db444a3855f9b995df8421bb6a94c4008d2
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-04-27 23:35:18 -0300

    style: adjust style in the nav

--- library-project/src/components/NavBar.tsx
+++ library-project/src/components/NavBar.tsx
@@ -1,18 +1,17 @@
-import { CiMenuFries } from "react-icons/ci";
 import { BiSolidBookHeart } from "react-icons/bi";
 import logo from "../assets/images/logo.png";
 import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav className="flex w-full justify-between items-center p-10 h-[105px] bg-darkslategray text-white tracking-widest">
+    <nav className="flex w-full justify-between items-center p-2 sm:p-10 h-[105px] bg-darkslategray text-white tracking-widest">
      
       <NavLink to="/">
         <div>
           <img
             src={logo}
             alt="Logo"
-            className="w-[70px] rounded-r-full rounded-l-full"
+            className="w-[70px] min-w-[70px] rounded-full"
           />
         </div>
       </NavLink>
@@ -25,15 +24,11 @@ export const NavBar = () => {
         </NavLink>
 
         <NavLink to="/favorites"  className={({ isActive }) => isActive ? 'active' : ''}>
-        <div className="flex px-10 items-center">
+        <div className="flex px-2 sm:px-10 items-center">
           <BiSolidBookHeart className="w-7 h-7 mr-1" />
           <p className="leading-none">Favoritos</p>
         </div>
         </NavLink>
-
-        <div>
-          <CiMenuFries className="w-7 h-7" />
-        </div>
 
       </div>
     </nav>
