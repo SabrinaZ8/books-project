341a5528c29899220d31f81576d1b978c2c6921e
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-07 01:01:28 -0300

    feat: adding functionality for footer input

--- library-project/src/components/Footer.tsx
+++ library-project/src/components/Footer.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
+  const [inputSearch, setInputSearch] = useState<string>("");
+  const navigate = useNavigate();
+
+  function handleSearch() {
+    navigate(`/listbooks?q=${encodeURIComponent(inputSearch)}`);
+  }
+  
   return (
     <div className="bg-darkslategray max-sm:min-h-[70vh] max-lg:min-h-[50vh] sm:h-[30vh] flex justify-around items-center flex-col text-gainsboro">
       <div className="flex justify-around items-center w-full h-[90%] flex-wrap flex-col sm:flex-row">
@@ -22,8 +31,16 @@ export const Footer = () => {
         <div className="my-10 relative">
           <p className="font-playfair tracking-widest text-xl mb-5 text-start sm:text-end">Procure por livros</p>
           <div className="flex items-center">
-            <input type="text" placeholder="Digite aqui..." className="border-b-2 border-white w-2xs focus:outline-none" />
-            <IoSearch className="w-8 h-8 bg-orangered p-1 rounded-md ml-3" />
+            <input 
+              type="text"
+              name="book-search"
+              aria-label="Digite um livro"
+              placeholder="Digite aqui..."
+              value={inputSearch}
+              onChange={(e) => setInputSearch(e.target.value)}
+              className="border-b-2 border-white w-2xs focus:outline-none" 
+            />
+            <IoSearch className="w-8 h-8 bg-orangered p-1 rounded-md ml-3" onClick={handleSearch} />
           </div>
         </div>
       </div>
