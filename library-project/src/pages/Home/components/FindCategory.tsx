7a028318bc45eb0d42956365b125c9e52d472df0
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-15 23:52:43 -0300

    style: change style in the categories

--- library-project/src/pages/Home/components/FindCategory.tsx
+++ library-project/src/pages/Home/components/FindCategory.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const FindCategory = () => {
   return (
-    <div className="flex justify-evenly items-center h-full overflow-x-auto">
+    <div className="flex justify-evenly items-center h-1/2 lg:h-full overflow-x-auto">
       <Link to={"/listbooks?subject=fiction"} className="find-category">
         <h2>Ficção</h2>
       </Link>
