693e86b63aab5363a6709aebc7d13f91bee3a1ae
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-18 00:54:02 -0300

    feat: add links to the footer

--- library-project/src/components/Footer.tsx
+++ library-project/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Footer = () => {
   const [inputSearch, setInputSearch] = useState<string>("");
@@ -13,34 +13,54 @@ export const Footer = () => {
   return (
     <div className="bg-darkslategray max-sm:min-h-[70vh] max-lg:min-h-[50vh] sm:h-[30vh] flex justify-around items-center flex-col text-gainsboro">
       <div className="flex justify-around items-center w-full h-[90%] flex-wrap flex-col sm:flex-row">
-        <div className="flex justify-center w-full lg:w-auto">
-          <img
-            src="/images/logo.webp"
-            alt="Logo"
-            className="w-[100px] rounded-r-full rounded-l-full opacity-80"
-          />
-        </div>
+        <Link to="/">
+          <div className="flex justify-center w-full lg:w-auto">
+            <img
+              src="/images/logo.webp"
+              alt="Logo"
+              className="w-[100px] rounded-r-full rounded-l-full opacity-80"
+            />
+          </div>
+        </Link>
+
         <div className="text-center">
           <ul className="ul-footer">
-            <li>Início</li>
-            <li>Sobre</li>
-            <li>Favoritos</li>
-            <li>Open Library</li>
+            <li>
+              <Link to="/">Início</Link>
+            </li>
+            <li>
+              <Link to="/favorites">Favoritos</Link>
+            </li>
+            <li>Creditos</li>
+            <li>
+              <a
+                href="https://openlibrary.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open Library
+              </a>
+            </li>
           </ul>
         </div>
         <div className="my-10 relative">
-          <p className="font-playfair tracking-widest text-xl mb-5 text-start sm:text-end">Procure por livros</p>
+          <p className="font-playfair tracking-widest text-xl mb-5 text-start sm:text-end">
+            Procure por livros
+          </p>
           <div className="flex items-center">
-            <input 
+            <input
               type="text"
               name="book-search"
               aria-label="Digite um livro"
               placeholder="Digite aqui..."
               value={inputSearch}
               onChange={(e) => setInputSearch(e.target.value)}
-              className="border-b-2 border-white w-2xs focus:outline-none" 
+              className="border-b-2 border-white w-2xs focus:outline-none"
             />
-            <IoSearch className="w-8 h-8 bg-orangered p-1 rounded-md ml-3" onClick={handleSearch} />
+            <IoSearch
+              className="w-8 h-8 bg-orangered p-1 rounded-md ml-3"
+              onClick={handleSearch}
+            />
           </div>
         </div>
       </div>
