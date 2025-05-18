835d1be94bee201fb70b265ef1b69342fded3f11
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-18 00:54:16 -0300

    feat: add scroll to top

--- library-project/src/routes/index.tsx
+++ library-project/src/routes/index.tsx
@@ -3,10 +3,12 @@ import { Favorites } from '../pages/Favorites/Favorites';
 import { Home } from '../pages/Home/Home';
 import { ListBooks } from '../pages/ListBooks/ListBooks';
 import { BookPage } from '../pages/BookPage/BookPage';
+import ScrollToTop from '../components/ScrollToTop';
 
 export const AppRoutes = () => {
     return (
       <BrowserRouter basename='/'>
+        <ScrollToTop />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/favorites" element={<Favorites />} />
